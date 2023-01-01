c8d098c309342c1930c2612d449bfbcb011c8032
Yeojun Yun <52143231+Yun024@users.noreply.github.com>
2023-01-01 18:25:54 +0900

    Update and rename T1-2. 이상치를 찾아라(소수점 나이).md to T1-2. 이상치를 찾아라(소수점 나이).py

--- Kaggel/T1-2. 이상치를 찾아라(소수점 나이).py
+++ Kaggel/T1-2. 이상치를 찾아라(소수점 나이).py
@@ -1,21 +1,17 @@
 # 라이브러리 및 데이터 불러오기
-```c
 import pandas as pd
 import numpy as np
 df = pd.read_csv("../input/bigdatacertificationkr/basic1.csv")
 df.head()
-```
+
 # 소수점 데이터 찾기
-```c
 df_flt=df[df["age"].astype("int64") - df["age"]!=0]
-```
+
 # 이상치를 포함한 데이터 올림, 내림, 버림의 평균값 
-```c
 ceil = np.ceil(df["age"]).mean()
 floor = np.floor(df["age"]).mean()
 trunc = np.trunc(df["age"]).mean()
-```
+
 # 평균값 더한 다음 출력
-```c 
 ceil + trunc + floor 
-```
+
